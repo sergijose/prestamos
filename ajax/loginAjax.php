c91beb9317e27e2ab44e3e6b813262efae8799be
sergio jara <sergiojosel.j@gmail.com>
2020-04-17 23:59:21 -0500

    agregando login model controler y vista

---
+++ ajax/loginAjax.php
@@ -0,0 +1,15 @@
+<?php
+	$peticionAjax=true;
+	require_once "../config/APP.php";
+	
+	if(){
+
+	
+
+	}else{
+		session_start(['name'=>'SPM']);
+		session_unset();
+		session_destroy();
+		header("Location: ".SERVERURL."login/");
+		exit();
+	}
